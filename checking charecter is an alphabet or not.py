58ca2010a91cf7807d7f573aa4ffb1ac9d6b79ad
Phaneendra-Kumar-347 <60686018+Phaneendra-Kumar-347@users.noreply.github.com>
2020-02-05 16:17:13 +0530

    Update checking charecter is an alphabet or not.py

--- checking charecter is an alphabet or not.py
+++ checking charecter is an alphabet or not.py
@@ -1,17 +1,17 @@
-#program to check the given charecter is an alphabet or not
+#program to check the given character is an alphabet or not
 #using conditional operator only
-charecter=input("enter one charecter")
-if ord(charecter) >=65 and ord(charecter) <=90 or ord(charecter) >=97 and ord(charecter) <=122:
-    print("the given input is an alphabet",charecter)
+character=input("enter one character")
+if ord(character) >=65 and ord(character) <=90 or ord(character) >=97 and ord(character) <=122:
+    print("the given input is an alphabet",character)
 else:
-    print("the given input is not an alphabet",charecter)
+    print("the given input is not an alphabet",character)
 
 
 #without conditional operator
     
-charecter=input("enter one charecter")
-if charecter.isalpha():
-    print("the given input is an alphabet",charecter)
+character=input("enter one character")
+if character.isalpha():
+    print("the given input is an alphabet",character)
 else:
-    print("the given input is not an alphabet",charecter)
+    print("the given input is not an alphabet",character)
 
