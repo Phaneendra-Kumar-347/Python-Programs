6a8beecb2e1458d69b9ee1ec449d5ba08fe5dbbd
Phaneendra-Kumar-347 <60686018+Phaneendra-Kumar-347@users.noreply.github.com>
2020-02-07 20:42:41 +0530

    Update rotations of the bits in binary number.py

--- rotations of the bits in binary number.py
+++ rotations of the bits in binary number.py
@@ -1,4 +1,4 @@
-"""Write a program to rotate bits of a given number."""
+"""Write a program to rotate bits in clock wise direction of a given number."""
 num=int(input("enter a number"))
 list1=list(bin(num))
 list1=list1[2:]
@@ -8,6 +8,20 @@ for i in range(no_rotations):
     list1.append(x)
 list1="".join(list1)
 print("after total rotations the binary value is {}".format(list1))
+
+
+"""Write a program to rotate bits in anti clock wise direction of a given number."""
+
+num=int(input("enter a number"))
+list1=list(bin(num))
+list1=list1[2:]
+no_rotations=int(input("enter no.of rotations what you want"))
+for i in range(no_rotations):
+    x=list1.pop(-1)
+    list1.insert(0,x)
+list1="".join(list1)
+print("after total rotations the binary value is {}".format(list1))
+
 
 
 
