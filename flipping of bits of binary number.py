07d3c1cd60541ded417eac5d46e3151363585614
Phaneendra-Kumar-347 <60686018+Phaneendra-Kumar-347@users.noreply.github.com>
2020-02-07 20:13:23 +0530

    Update flipping of bits of binary number.py

--- flipping of bits of binary number.py
+++ flipping of bits of binary number.py
@@ -1,4 +1,13 @@
 "Write a program to flip bits of a binary number "
 "using bitwise operator."
+import math 
 num=int(input("enter a number"))
-~num
+def invertBits(num):  
+    x = int(math.log2(num)) + 1
+    print(x)
+    for i in range(x):  
+        num = (num ^ (1 << i))  
+    print(num)  
+  
+invertBits(num)  
+
