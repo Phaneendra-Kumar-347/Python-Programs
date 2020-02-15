8002056e3d1a084dd9f18b6796bf21706ec289fb
Phaneendra-Kumar-347 <60686018+Phaneendra-Kumar-347@users.noreply.github.com>
2020-02-15 17:07:32 +0530

    Add files via upload

---
+++ bubble sort assending and decending order.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 11:40:49 2020
+
+@author: colors
+"""
+#bubble sort
+list1=[5,4,7,3,9,2,8,1,6]
+print("list1:",list1)
+list2=[]
+k=0
+for j in range(len(list1)):
+    for i in range(len(list1)-1):
+        if list1[i]>list1[i+1]: # change to < and rewrite other program may be the chance decending order will be come.
+            list1[i],list1[i+1]= list1[i+1],list1[i]
+            
+    list2.append(list1[i+1+k])
+    k=k-1
+print(list1)
+print(list2)
+
+
+print("phani\thari")
+print("phani\rhari")
+print("phani\nhari")
